f4e2c8ccea04400df68094c4e31058ff226bf859
Eli Sacks <bastlifa@gmail.com>
2019-12-13 01:58:13 -0500

    rps done.. omg, why did that take so long

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,30 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  choices = [['rock'], ['paper'], ['scissors']]
 
+  if n == 0: return [[]]
+
+  cache = {
+    0: [[]],
+    1: [['rock'], ['paper'], ['scissors']]
+  }
+
+  def rps_inner(n):
+    
+    try:
+      if cache[n]:
+        return cache[n]
+    except:
+      cache[n] = []
+      for i in range(3**n):
+        cache[n].append(rps_inner(n-1)[i//3] + choices[i%3])
+      
+      return cache[n]
+  rps_inner(n)
+
+
+  return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
